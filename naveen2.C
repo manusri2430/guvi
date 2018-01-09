c3f9f86b64a4a2aaf118ab5f9fc074886b2ac0b8
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-09 16:50:25 +0530

    Create naveen2.C

---
+++ naveen2.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the numbers");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+{
+printf("a is big");
+}
+ifelse(b>c)
+{
+printf("b is big");
+}
+else
+{
+printf("c is big");
+}
+}
