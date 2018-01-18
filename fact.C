d3cc663233cd453f178eaa624c6b74315e180d15
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-18 07:51:07 -0800

    Create fact.C

---
+++ fact.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n,f,fact;
+printf("enter the number");
+scanf("%d",&n);
+for(int i=0;i<=n;i++)
+{
+fact=f*i;
+}
+printf("factorial",fact);
+}
