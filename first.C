415b3bcda192de6d2be4ff5d841e4ad2539ce0e7
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-18 16:25:26 +0530

    Create first.C

---
+++ first.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int i,n,k;
+printf("enterthefirst number");
+scanf("%d",n);
+printf("enter the second number ");
+scanf("%d",&k);
+for(i=k+1;i<=n;i++)
+{
+if(i%2!=0)
+{
+printf("%d",i);
+}
+}
