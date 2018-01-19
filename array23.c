5b6c4a66cdc06d8571be8aa8f8c5c763e7573fec
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-19 16:35:48 +0530

    Create array23.c

---
+++ array23.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+void main()
+
+{
+
+    int array[1000];
+
+    int i, j, n, temp;
+
+ 
+
+    printf("Enter the value of num \n");
+
+    scanf("%d", &n);
+
+    printf("Enter the elements one by one \n");
+
+    for (i = 0; i < n; i++)
+
+    {
+
+        scanf("%d", &array[i]);
+
+    }
+
+    printf("Input array is \n");
+
+    for (i = 0; i < n; i++)
+
+    {
+
+        printf("%d\n", array[i]);
+
+    }
+
+  
+
+    for (i = 0; i < n; i++)
+
+    {
+
+        for (j = 0; j < (n - i - 1); j++)
+
+        {
+
+            if (array[j] > array[j + 1])
+
+            {
+
+                temp = array[j];
+
+                array[j] = array[j + 1];
+
+                array[j + 1] = temp;
+
+            }
+
+        }
+
+    }
+
+    printf("Sorted array is...\n");
+
+    for (i = 0; i < n; i++)
+
+    {
+
+        printf("%d\n", array[i]);
+
+    }
+
+}
