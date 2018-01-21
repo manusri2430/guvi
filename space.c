e49a7b2f07070aed37ed3149ec8c43e00a7b5478
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-21 11:23:43 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[i];
+int i,n=100,count=0;
+printf("enter the string");
+scanf("%s",&s[i]);
+strlen(n);
+for(i=0;i<n;i++)
+{
+if(s[i]=='\0')
+{
+count=count++;
+}
+printf("space=%d",count);
+}
+}
