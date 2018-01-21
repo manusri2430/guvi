cdd9e7d3c3c23302e6c80422dc265c8fb5780ced
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-21 11:34:42 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+int i; 
+ int count=0;
+char s[100];
+scanf("%s",s);
+for(i=0;s[i]!='\0';i++)
+{
+	
+if(isdigit(s[i]))
+{
+count++;
+}
+}
+printf("count: %d ",count);
+return 0;
+}
