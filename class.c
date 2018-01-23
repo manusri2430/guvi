5ae975f52faa91a9a5bea5ba2a3128c56829548a
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-23 16:45:40 +0530

    Update and rename 53.c to class.c

--- class.c
+++ class.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-void main()
+int  main()
 {
-    int n, i, sum = 0;
+    int num, i, sum = 0;
     
     printf("Enter a positive integer: ");
-    scanf("%d",&n);
+    scanf("%d",&num);
 
-    for(i=1; i <= n; ++i)
+    for(i=1; i <= num; ++i)
     {
         sum += i;  
     }
 
     printf("Sum = %d",sum);
 
-   
+   return 0;
 }
