265a48e28978acef161b45648b8b8bc97c475fa3
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-23 16:23:01 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num,i,sumo=0;
+float avg;
+printf("enter the number ");
+scanf("%d",&num);
+for(i=0;i<num;i++)
+{
+
+sum=sum+num;
+}
+avg=sum/num;
+printf("%d",avg);
+}
