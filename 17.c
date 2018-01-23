c03c34363e5d184537a2df3e1014a5eb23e53dba
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-23 15:55:17 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int a[100]={"sanjeevi"};
+int b[100]={"naveen"};
+int i,j;
+if(strlen(a[i])>=strlen(b[j]))
+{
+printf("the string is",a[i]);
+}
+else
+{
+printf("the string is",b[j]);
+}
+}
