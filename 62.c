a78612517dfd0f182589b8229a554b89f0837adb
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-25 16:19:09 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+string s[100]={"10001"};
+int i,n=6;
+for(i=0;i<=n;i++)
+{
+if(s[i]=='0'||s[i]=='1')
+{
+printf("given string is binary");
+}
+else
+{
+printf("not a binary");
+}
+}
+}
