e7fc5527e22ef5d0b9f3008d55068626a9f852c2
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-01-25 16:39:44 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+void main()
+{
+	int n;
+	printf("\nEnter the number : ");
+	scanf("%d",&n);
+	if(n%7==0)
+	{
+		printf("\n%d is the multiple of 7 ",n);
+	}
+	else
+	{
+		printf("\n%d is not the multiple of 7 ",n);
+	}
+	
+}
