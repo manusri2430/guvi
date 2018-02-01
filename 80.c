811e2461251f1229e9f495b828a108d9a87a20d3
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-02-01 10:33:14 +0000

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char n[20];
+int a=0,i;
+printf("enter n value");
+scanf("%d",&n);
+a=strlen(n[i]);
+for(i=0;i<a;i++)
+{
+if((a[i]%2)==0)
+{
+printf("%d",a[i]);
+}
+}
+}
