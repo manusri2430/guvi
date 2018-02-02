ec50ae58080c45ec27f2e2a691f863ad12091fbe
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-02-02 08:51:47 +0000

    Update and rename 85.c to 1.c

---
+++ 1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+  char ch[50];
+  int l,i;
+  printf("Enter a string");
+  scanf("%s",ch);
+  l=strlen(ch);
+  for(i=0;i<=l;i++)
+  {
+    if(i%2==0)
+    {
+      printf("%c",ch[i]);
+    }
+  }
+  printf("\t");
+  for(i=0;i<=l;i++)
+  {
+    if(i%2!=0)
+    {
+      printf("%c",s[i]);
+    }
+  }
+  
+  
+}
