3e385fb8876d5a46f05a85388ca1d2a9c9f1610e
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-02-02 08:39:08 +0000

    Create 85.c

---
+++ 85.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+char s[20],a[20];
+int i;
+printf("enter the string\n");
+scanf("%s",&s);
+l=strlen(s);
+for(i=0;i<=l;i++)
+{
+if(i%2==o)
+{
+printf("%s",s[i]);
+}
+}
+for(i=0;i<=l;i++)
+{
+if(i%2!=0)
+{
+printf("%f",a[i]);
+}
+}
+}
