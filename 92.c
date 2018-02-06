6fbcead94a5cfbfb8d3314150ca1b73b3d66eda7
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-02-06 08:41:20 +0000

    Create 92.c

---
+++ 92.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+inta[100],i,n,sum=0;
+printf("enter the n value\n");
+scanf("%d",&n);
+printf("enter the array");
+for(i=0;i<=n;i++)
+{
+
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("sum %d",sum);
+}
+
+
+
