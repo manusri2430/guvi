40516f1ba2488eeb32f00429085461282336e9ab
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2018-02-08 08:56:09 +0000

    Update 111.c

--- 111.c
+++ 111.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main()
 {
-int n,c=0;
+int n,c=0,r;
 printf("enter the num\n");
 scanf("%d",&n);
 while(n!=0)
 {
 r=n%10;
-c=c++;
+c=c+1;
 n=n/10;
 }
 printf("result is %d",c);
